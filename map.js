a67047a9971a4be7c6520cde04b2bced88cf83b4
unknown <vron371@gmail.com>
2022-12-17 12:41:25 +0545

    first commit

---
+++ map.js
@@ -0,0 +1,153 @@
+$(function () {
+        var origin, destination, map;
+        var myLatLng = { lat: 38.3460, lng: -0.4907 };
+        var mapOptions = {
+            center: myLatLng,
+            zoom: 7,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+
+        };
+
+//create map
+var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+//create a DirectionsService object to use the route method and get a result for our request
+var directionsService = new google.maps.DirectionsService();
+//create a DirectionsRenderer object which we will use to display the route
+var directionsDisplay = new google.maps.DirectionsRenderer();
+//bind the DirectionsRenderer to the map
+directionsDisplay.setMap(map);
+
+        
+var from_places = new google.maps.places.Autocomplete(document.getElementById('from_places'));
+var to_places = new google.maps.places.Autocomplete(document.getElementById('to_places'));
+
+google.maps.event.addListener(from_places, 'place_changed', function () {
+    var from_place = from_places.getPlace();
+    var from_address = from_place.formatted_address;
+    $('#origin').val(from_address);
+});
+
+google.maps.event.addListener(to_places, 'place_changed', function () {
+    var to_place = to_places.getPlace();
+    var to_address = to_place.formatted_address;
+    $('#destination').val(to_address);
+});
+
+
+     
+
+        function displayRoute(travel_mode, origin, destination, directionsService, directionsDisplay) {
+            directionsService.route({
+                origin: origin,
+                destination: destination,
+                travelMode: travel_mode,
+                avoidTolls: true
+            }, function (response, status) {
+                if (status === 'OK') {
+                    directionsDisplay.setMap(map);
+                    directionsDisplay.setDirections(response);
+                } else {
+                    directionsDisplay.setMap(null);
+                    directionsDisplay.setDirections(null);
+                    alert('Could not display directions due to: ' + status);
+                }
+            });
+        }
+
+        // calculate distance , after finish send result to callback function
+        function calculateDistance(travel_mode, origin, destination) {
+
+            var DistanceMatrixService = new google.maps.DistanceMatrixService();
+            DistanceMatrixService.getDistanceMatrix(
+                {
+                    origins: [origin],
+                    destinations: [destination],
+                    travelMode: google.maps.TravelMode[travel_mode],
+                    unitSystem: google.maps.UnitSystem.IMPERIAL, // miles and feet.
+                    // unitSystem: google.maps.UnitSystem.metric, // kilometers and meters.
+                    avoidHighways: false,
+                    avoidTolls: false
+                }, save_results);
+        }
+
+        // save distance results
+        function save_results(response, status) {
+
+            if (status != google.maps.DistanceMatrixStatus.OK) {
+                $('#result').html(err);
+            } else {
+                var origin = response.originAddresses[0];
+                var destination = response.destinationAddresses[0];
+                if (response.rows[0].elements[0].status === "ZERO_RESULTS") {
+                    $('#result').html("Sorry , not available to use this travel mode between " + origin + " and " + destination);
+                } else {
+                    var distance = response.rows[0].elements[0].distance;
+                    var duration = response.rows[0].elements[0].duration;
+                    var distance_in_kilo = distance.value / 1000; // the kilo meter
+                    var distance_in_mile = distance.value / 1609.34; // the mile
+                    var duration_text = duration.text;
+                    appendResults(distance_in_kilo, distance_in_mile, duration_text);
+                    appendResults1(distance_in_kilo, distance_in_mile, duration_text);
+                    sendAjaxRequest(origin, destination, distance_in_kilo, distance_in_mile, duration_text);
+                }
+            }
+        }
+
+        // append html results
+        function appendResults(distance_in_kilo, distance_in_mile, duration_text) {
+            $("#result").removeClass("hide");
+            $('#in_mile').html(" Distance in Mile: " + distance_in_mile.toFixed(2));
+            $('#in_kilo').html("Distance in KM: " + distance_in_kilo.toFixed(2));
+            $('#duration_text').html("Duration: " + duration_text);
+        }
+
+
+        function appendResults1(distance_in_kilo, distance_in_mile, duration_text) {
+            $("#result").removeClass("hide");
+            $('#in_mile1').val(distance_in_mile.toFixed(2));
+            $('#in_kilo1').val(distance_in_kilo.toFixed(2));
+            $('#duration_text1').val(duration_text.toFixed(2));
+        }
+
+
+        // send ajax request to save results in the database
+        
+
+        // on submit  display route ,append results and send calculateDistance to ajax request
+        $('#distance_form').submit(function (e) {
+            e.preventDefault();
+            var origin = $('#origin').val();
+            var destination = $('#destination').val();
+            var travel_mode = $('#travel_mode').val();
+            var directionsDisplay = new google.maps.DirectionsRenderer({'draggable': false});
+            var directionsService = new google.maps.DirectionsService();
+           displayRoute(travel_mode, origin, destination, directionsService, directionsDisplay);
+            calculateDistance(travel_mode, origin, destination);
+        });
+
+    });
+
+    // get current Position
+    function getCurrentPosition() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(setCurrentPosition);
+        } else {
+            alert("Geolocation is not supported by this browser.")
+        }
+    }
+
+    // get formatted address based on current position and set it to input
+    function setCurrentPosition(pos) {
+        var geocoder = new google.maps.Geocoder();
+        var latlng = {lat: parseFloat(pos.coords.latitude), lng: parseFloat(pos.coords.longitude)};
+        geocoder.geocode({ 'location' :latlng  }, function (responses) {
+            console.log(responses);
+            if (responses && responses.length > 0) {
+                $("#origin").val(responses[1].formatted_address);
+                $("#from_places").val(responses[1].formatted_address);
+                //    console.log(responses[1].formatted_address);
+            } else {
+                alert("Cannot determine address at this location.")
+            }
+        });
+    }
